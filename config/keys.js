ec42783938e5cff43cc969ef46dc3d9eb323c476
lioent <59694379+lioent@users.noreply.github.com>
2020-06-11 00:21:16 -0300

    Connect the backend with the DialogFlow, tests made with Servistate

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    googleProjectId: 'proto-bot01-jwmphe',
+    dialogFlowSessionsId: 'proto-bot-session',
+    dialogFlowSessionLanguageCode: 'en-US'
+}
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send({ 'hello': 'there' });
-});
+require('./routes/dialogFlowRoutes')(app); 
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
