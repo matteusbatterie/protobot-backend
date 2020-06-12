dfb29be76fc6afec69117ce64f674900026c706c
lioent <59694379+lioent@users.noreply.github.com>
2020-06-12 09:15:33 -0300

    Fixes

--- routes/dialogFlowRoutes.js
+++ routes/dialogFlowRoutes.js
@@ -6,7 +6,10 @@ module.exports = app => {
     });
 
     app.post('/api/df_text_query', async (req, res) => {
-        let responses = await chatbot.textQuery(req.body.text, req.body.parameters);
+        let responses = await chatbot.textQuery(req.body.text, req.body.parameters)
+            .catch(err => {
+                throw new Error(err);
+            });
         res.send(responses[0].queryResult);
     });
 
