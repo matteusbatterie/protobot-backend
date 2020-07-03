a280f238611f918ffe189ad30c19c5d6e97bec24
Marcos Bueno <marcos@mplb.com.br>
2020-07-02 21:58:14 -0300

    Add cors to accept localhost requests

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors({
+        origin: 'http://localhost:3000',
+        optionsSuccessStatus: 200
+}))
 
 require('./routes/dialogFlowRoutes')(app); 
 console.log(process.env)
