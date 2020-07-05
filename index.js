64df3ebb8a942947161fe40d469406d2af2d2f7c
lioent <59694379+lioent@users.noreply.github.com>
2020-07-05 13:36:46 -0300

    Code cleanup

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(bodyParser.json());
 
 require('./routes/dialogFlowRoutes')(app); 
-console.log(process.env)
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
